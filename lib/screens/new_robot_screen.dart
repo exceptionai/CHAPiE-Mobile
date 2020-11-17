fd37408d787f1959a6bc4a18c0a980ec49dcaca8
gabriel <gabriellopes112009@gmail.com>
2020-11-17 14:48:38 -0300

    front da tela new robo

--- lib/screens/new_robot_screen.dart
+++ lib/screens/new_robot_screen.dart
@@ -1,13 +1,12 @@
 import 'package:FiapEx/components/app_bar_fiap_ex.dart';
 import 'package:FiapEx/components/drawer_fiap_ex.dart';
+import 'package:FiapEx/components/robot_form_ex.dart';
 import 'package:FiapEx/models/robot.dart';
 import 'package:FiapEx/services/charpie_service.dart';
-import 'package:FiapEx/tiles/robot_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class NewRobotScreen extends StatefulWidget {
-
   @override
   _NewRobotScreenState createState() => _NewRobotScreenState();
 }
@@ -15,60 +14,82 @@ class NewRobotScreen extends StatefulWidget {
 class _NewRobotScreenState extends State<NewRobotScreen> {
   CharpieService service = CharpieService();
 
+  final _form = const [
+    {
+      'text': 'Qual será o nome do robô?'
+    },
+    {
+      'text': 'Qual será o tipo do robô?',
+      'options': [
+        {'text': 'Android', 'value': 'android'},
+        {'text': 'Babysister', 'value': 'babysyster'},
+        {'text': 'Military', 'value': 'military'},
+        {'text': 'Worker', 'value': 'worker'}
+      ],
+    },
+    {
+      'text': 'Selecione um esquema para manufatura do robô?',
+      'options': [
+        {'text': 'Ousadasso', 'value' : 'ousadasso'},
+        {'text': 'Super ousadao', 'value' : 'super ousado'},
+        {'text': 'Cuzudo', 'value' : 'cuzudo'},
+        {'text': 'Upload de esquema','value' : 'upload'},
+      ],
+    },
+    {
+      'text': 'Deseja concluir a construção?',
+      'options': [
+        {'text': 'Sim', 'value' : 1},
+        {'text': 'Não', 'value' : 0},
+      ],
+    },
+  ];
+  
+  var _currentField = 0;
+  var _robot = RobotModel();
+
+  void _next(){
+      setState(() {
+        if(_currentField >= _form.length - 1){
+          _currentField = _form.length - 1;
+        }else{
+           _currentField ++;
+        }
+      });
+    }
+
+    void _previous(){
+      setState(() {
+        if(_currentField != 0){
+          _currentField --;
+        }
+          
+      });
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarFiapEx(
-        action: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: InkWell(
-          child: Image.asset('assets/images/entregatrabalhos.png', height: 26),
-          onTap: () {
-            Navigator.of(context).pushReplacementNamed('/scheme');
-          },
+        resizeToAvoidBottomInset: false,
+        appBar: AppBarFiapEx(
+            action: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InkWell(
+            child:
+                Image.asset('assets/images/entregatrabalhos.png', height: 26),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/scheme');
+            },
+          ),
+        )),
+        drawer: DrawerFiapEx(
+          route: '/',
         ),
-      )),
-      drawer: DrawerFiapEx(
-        route: '/',
-      ),
-      body: Container(
-          padding: EdgeInsets.all(8.0),
-          color: Theme.of(context).accentColor,
-          width: MediaQuery.of(context).size.width,
-          child: Container(
-            width:  50,
-            margin: EdgeInsets.only(top: 220.0),
-            child: Column (
-              children: [
-                Text("Como gostaria de chamar seu robô"),
-                Container(
-                  margin: EdgeInsets.only(top: 50),
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Theme.of(context).primaryColor), 
-                  ),
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColorLight,                      
-                    ),
-                    validator: (value) {
-                      if(value.isEmpty) {
-                        return "Campo inválido";
-                      }
-                      return null;
-                    },
-                    cursorColor: Theme.of(context).primaryColor,
-                    cursorHeight: 25.0,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      
-                    )
-                  ),
-                ),
-            ],),
-          )
-              
-    ));
+        body: RobotForm(
+        textField: _form[_currentField]['text'],
+        fieldOptions: _form[_currentField]['options'],
+        next: _next,
+        previous: _previous),
+      );
   }
 }
